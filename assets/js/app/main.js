16a79de05d7bfb4a24b02353999129d965df5e52
pedrodif <pedrodif@icloud.com>
2025-01-23 00:07:32 -0300

    refactor/renomeia pastas para seguir padrão

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,11 +1,11 @@
-import { FeedbackView } from '../view/feedback/main.js'
 import { FeedbackModel } from '../model/feedbackModel.js'
+import { FeedbackView } from '../view/feedbackView/main.js'
 import { FeedbackController } from '../controller/feedbackController.js'
 import { FeedbackRepository } from '../repository/feedbackRepository.js'
 
-const feedbackView = new FeedbackView()
 const feedbackRepository = new FeedbackRepository()
 const feedbackModel = new FeedbackModel(feedbackRepository)
+const feedbackView = new FeedbackView()
 const feedbackController = new FeedbackController(feedbackModel, feedbackView)
 
 document.addEventListener('DOMContentLoaded', () => {
