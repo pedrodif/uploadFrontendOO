4659cb2c7a02d5aa35eaababb5477d555a4455e1
pedrodif <pedrodif@icloud.com>
2025-01-30 01:24:56 -0300

    feat/evolui implementação da classe FeedbackDetalhesController

--- js/controller/FeedbackController.js
+++ js/controller/FeedbackController.js
@@ -1,15 +1,15 @@
 import { Url } from '../utils/Url.js'
 import { Notificador } from '../utils/Notificador.js'
 import { RequestHelper } from '../utils/RequestHelper.js'
-
-import { FeedbackView } from '../view/feedback/FeedbackView.js'
-import { FeedbackService } from '../service/FeedbackService.js'
-
 export class FeedbackController {
-    constructor(feedbackView) {
-        this.feedbackView = new FeedbackView()
-        this.feedbackService = new FeedbackService()
+    constructor(feedbackView, feedbackService) {
+        this.feedbackView = feedbackView
+        this.feedbackService = feedbackService
         this.feedbackRecuperado = null
+    }
+
+    getFeedbackView() {
+        return this.feedbackView
     }
 
     async listarFeedbacks() {
@@ -20,39 +20,7 @@ export class FeedbackController {
         )
 
         Notificador.resposta(resposta, 'listar')
-        if (resposta) {
-            this.feedbackView.listarFeedbacks(resposta, this.gerenciarForm.bind(this))
-        }
-    }
-
-    async criar(feedback) {
-        const { colaboradorId, gestorId, monitorId } = Url.consultarParametros()
-
-        const resposta = await RequestHelper.executar(
-            () => this.feedbackService.criar({ ...feedback, colaboradorId, gestorId: gestorId ?? monitorId })
-        )
-
-        Notificador.resposta(resposta, 'criar')
-        if(resposta) {
-            this.feedbackView.atualizarListaFeedbacks(resposta, this.gerenciarForm.bind(this))
-            this.gerenciarForm('detalhes')
-        }
-    }
-
-    async atualizar(feedbackAtualizado) {
-        const { id } = this.feedbackRecuperado
-
-        const resposta = await RequestHelper.executar(
-            () => this.feedbackService.atualizar(id, this.feedbackRecuperado, feedbackAtualizado)
-        )
-
-        Notificador.resposta(resposta, 'atualizar')
-        if (resposta) {
-            Url.adicionarParametroURL('modo', 'criar')
-            Url.removerParametroURL('feedbackId')
-            this.listarFeedbacks('detalhes')
-            this.gerenciarForm('detalhes')
-        }
+        return resposta
     }
 
     async recuperarFeedbackPorId() {
@@ -63,8 +31,28 @@ export class FeedbackController {
         )
 
         Notificador.resposta(this.feedbackRecuperado, 'recuperar')
-        if(this.feedbackRecuperado) {
-            this.feedbackView.getForm().definirValoresIniciais(this.feedbackRecuperado)
-        }
+        return this.feedbackRecuperado
+    }
+
+    async criar(feedback) {
+        const { colaboradorId, gestorId, monitorId } = Url.consultarParametros()
+
+        const resposta = await RequestHelper.executar(
+            () => this.feedbackService.criar({ ...feedback, colaboradorId, gestorId: gestorId ?? monitorId })
+        )
+
+        Notificador.resposta(resposta, 'criar')
+        return resposta
+    }
+
+    async atualizar(feedbackAtualizado) {
+        const { id } = this.feedbackRecuperado
+
+        const resposta = await RequestHelper.executar(
+            () => this.feedbackService.atualizar(id, this.feedbackRecuperado, feedbackAtualizado)
+        )
+
+        Notificador.resposta(resposta, 'atualizar')
+        return resposta
     }
 }
--- js/controller/FeedbackDetalhesController.js
+++ js/controller/FeedbackDetalhesController.js
@@ -1,22 +1,58 @@
-import { FeedbackController } from "./FeedbackController";
+import { Url } from "../utils/Url.js"
+
+import { FeedbackController } from "./FeedbackController"
+import { FeedbackView } from "../view/FeedbackView.js"
+import { FeedbackService } from "../service/FeedbackService.js"
 
 export class FeedbackDetalhesController extends FeedbackController {
-    constructor(parameters) {
-        super(new FeedbackView())
+    constructor() {
+        super(new FeedbackView(), new FeedbackService())
     }
 
-    gerenciarForm(variante = 'perfil') {
+    gerenciarForm() {
         const { modo } = Url.consultarParametros()
 
         switch (modo) {
             case 'detalhes':
                 this.recuperarFeedbackPorId()
-                if (variante == 'detalhes') this.feedbackView.getForm().submit(this.atualizar.bind(this))
+                this.feedbackView.getForm().submit(this.atualizar.bind(this))
                 break
             default:
                 this.feedbackView.getForm().definirValoresIniciais({ dataCriacao: Utils.gerenciarData() })
                 this.feedbackView.getForm().submit(this.criar.bind(this))
                 break
+        }
+    }
+
+    async listarFeedbacks() {
+        const resposta = await super.listarFeedbacks()
+        if (resposta) {
+            this.getFeedbackView().listarFeedbacks(resposta, this.gerenciarForm.bind(this))
+        }
+    }
+
+    async criar(feedback) {
+        const resposta = await super.criar(feedback)
+        if (resposta) {
+            this.getFeedbackView().atualizarListaFeedbacks(resposta, this.gerenciarForm.bind(this))
+            this.gerenciarForm()
+        }
+    }
+
+    async atualizar(feedbackAtualizado) {
+        const resposta = await super.atualizar(feedbackAtualizado)
+        if (resposta) {
+            Url.adicionarParametroURL('modo', 'criar')
+            Url.removerParametroURL('feedbackId')
+            this.listarFeedbacks()
+            this.gerenciarForm()
+        }
+    }
+
+    async recuperarFeedbackPorId() {
+        const resposta = await super.recuperarFeedbackPorId()
+        if (resposta) {
+            this.getFeedbackView().getForm().definirValoresIniciais(resposta)
         }
     }
 }
