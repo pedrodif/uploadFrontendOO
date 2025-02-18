2973135df078bd45bff8b05bbbce613f752e5bab
pedrodif <pedrodif@icloud.com>
2025-02-18 13:33:24 -0300

    refactor/melhora testes do Form ao reduzir a repetição de codigo

--- tests/view/components/Form.test.js
+++ tests/view/components/Form.test.js
@@ -3,8 +3,7 @@ import { Form } from '../../../js/view/components/Form.js'
 
 describe('Form', () => {
     let form
-
-    beforeEach(() => {
+    beforeAll(() => {
         const inputNome = Utils.criarElemento('input')
         inputNome.type = 'text'
         inputNome.name = 'nome'
@@ -55,20 +54,16 @@ describe('Form', () => {
 
     describe('Gerenciar campo obrigatório', () => {
         let input
-
-        beforeEach(() => {
+        beforeAll(() => {
             input = Utils.consultarSeletor('#nome')
-            input.classList.remove('input-erro-campo-obrigatorio')
-            input.nextElementSibling.remove()
+            form.gerenciarCampoObrigatorio(input, '')
         })
 
         test('Deve adicionar classe de erro quando campo obrigatório não for preenchido', () => {
-            form.gerenciarCampoObrigatorio(input, '')
             expect(input.classList.contains('input-erro-campo-obrigatorio')).toBeTruthy()
         })
 
         test('Deve adicionar mensagem de erro quando o campo obrigatório não for preenchido', () => {
-            form.gerenciarCampoObrigatorio(input, '')
             expect(input.nextElementSibling.tagName).toBe('P')
             expect(input.nextElementSibling.textContent).toBe('Este campo é obrigatório *')
         })
@@ -80,19 +75,20 @@ describe('Form', () => {
 
     describe('Submit', () => {
         let callbackMock
-        beforeEach(() => callbackMock = jest.fn())
+        beforeEach(() => {
+            callbackMock = jest.fn()
+            form.onSubmit(callbackMock)
+        })
 
         test('Deve chamar a callback passada no método onSubmit', () => {
             form.validarForm = jest.fn().mockReturnValue(true)
-            form.onSubmit(callbackMock)
-
+          
             Utils.consultarSeletor('#form').dispatchEvent(new Event('submit'))
             expect(callbackMock).toHaveBeenCalled()
         })
 
         test('Não deve chamar a callback passada no método onSubmit', () => {
             form.validarForm = jest.fn().mockReturnValue(false)
-            form.onSubmit(callbackMock)
 
             Utils.consultarSeletor('#form').dispatchEvent(new Event('submit'))
             expect(callbackMock).not.toHaveBeenCalled()
